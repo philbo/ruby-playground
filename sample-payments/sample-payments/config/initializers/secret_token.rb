f2c92c943cb888fcea09aabaf0f85d95a42c59d6
philbo <philbo@gmail.com>
2014-01-07 11:42:18 +0000

    created rails app

---
+++ sample-payments/sample-payments/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SamplePayments::Application.config.secret_key_base = 'ec781be768b9c047b16fad2754fb4fa6662d9445db4c9e20f22a9c55b324a050483611aa08b04637dd7a44c18a4f49941858511e69cf030c0fdd929761a57323'
