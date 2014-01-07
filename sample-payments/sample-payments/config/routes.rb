cebfe788d17fc9c375137d007a8acdbb6fa78f34
philbo <philbo@gmail.com>
2014-01-07 12:15:04 +0000

    created account controller

--- sample-payments/sample-payments/config/routes.rb
+++ sample-payments/sample-payments/config/routes.rb
@@ -1,4 +1,5 @@
 SamplePayments::Application.routes.draw do
+  get "account/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
