5f5b6722330242c7ee469fb95d95fca2cf7bb1e7
philbo <philbo@gmail.com>
2014-01-08 17:46:31 +0000

    added Koala gem for FB interaction. Adding simple posting and listing of posts

--- post-sharing/post-sharing.rb
+++ post-sharing/post-sharing.rb
@@ -1,6 +1,35 @@
 require "sinatra"
 require "sinatra/reloader"
+require "koala"
+require "json"
 
 get "/" do
+	if params[:oauth_access_token]
+		begin
+			@graph = Koala::Facebook::API.new(params[:oauth_access_token])
+
+			@fb_feed = @graph.get_connections("me", "feed")
+
+			erb :index, :locals => {:text => "Hello, Bob Diamond"}
+		rescue => exception
+	  		return exception.to_s
+		end
+	end
+
 	erb :index, :locals => {:text => "Hello, Bob Diamond"}
+end
+
+post "/share" do
+	oauth_access_token = params["sharing-authtoken"]
+	text_to_post = params["sharing-text"]
+
+	begin
+		@graph = Koala::Facebook::API.new(oauth_access_token)
+
+		response = @graph.put_connections("me", "feed", :message => text_to_post)
+
+		redirect to("/?oauth_access_token=#{oauth_access_token}")
+	rescue => exception
+  		return exception.to_s
+	end
 end
