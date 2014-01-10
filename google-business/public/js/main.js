a656667c1339217a423e7a6f0ae82aeb5a065567
philbo <philbo@gmail.com>
2014-01-10 00:52:16 +0000

    added Google Maps integration and some wonky JS

--- google-business/public/js/main.js
+++ google-business/public/js/main.js
@@ -9,9 +9,10 @@ function init() {
 			url : formURL,
 			type: "POST",
 			data : postData,
-			success:function(data, textStatus, jqXHR) 
+			success: function(data, textStatus, jqXHR) 
 			{
 				var jsonData = JSON.parse(data);
+				console.log(jsonData);
 				if(jsonData.status === "ZERO_RESULTS") {
 					$("#search-results").html("No results found");
 				} else {
@@ -27,14 +28,57 @@ function init() {
 	});
 }
 
+function getDetails(reference) {
+	$.ajax(
+	{
+		url : "/details?reference=" + reference,
+		type: "GET",
+		success: function(data, textStatus, jqXHR) 
+		{
+			var jsonData = JSON.parse(data);
+			if(jsonData.status === "ZERO_RESULTS") {
+				$("#search-results").html("No results found");
+			} else {
+				var location = jsonData["result"]["geometry"]["location"];
+				drawMap(location["lat"], location["lng"]);
+			}
+		},
+		error: function(jqXHR, textStatus, errorThrown) 
+		{
+			//if fails      
+		}
+	});
+	return false;
+}
+
 function renderResults(data) {
 	var output = "<ul>";
+	var location = "";
 	$.each(data["predictions"], function(i, item) {
-		output += "<li><a href='/details?reference=" + item["reference"] + "'>" + item["description"] + "</a></li>";
+		output += "<li><a onclick='getDetails(\"" + item["reference"] + "\")'>" + item["description"] + "</a></li>";
 	});
 	output += "</ul>";
 
 	$("#search-results").html(output);
 }
 
+function drawMap(lat, lng) {
+	var myLatlng = new google.maps.LatLng(lat, lng);
+	var mapOptions = {
+	  zoom: 16,
+	  center: myLatlng
+	}
+	var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+	// To add the marker to the map, use the 'map' property
+	var marker = new google.maps.Marker({
+	    position: myLatlng,
+	    map: map,
+	    title:"Hello World!"
+	});
+
+	//var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+}
+
+//google.maps.event.addDomListener(window, 'load', drawMap);
 $(document).ready(init);
